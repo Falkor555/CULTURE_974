7e32895530a0c29de75ba169b301a9388077be76
Arnaud BIDEL <abidel.cda@FORMATION.LOCAL>
2026-01-14 14:41:37 +0400

    Sauvegarde avant mise à jour

---
+++ culture_974/src/Controller/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'app_categories')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    public function navbar(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/_navbar.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+}
